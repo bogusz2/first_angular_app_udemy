40f75a508140f1f4e03ae52453050ba4bfea68f6
pboguszewicz <pboguszewicz@bluesoft.net.pl>
2019-02-17 12:57:04 +0100

    Solution of first task of udemy course

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+
+export class WarningAlertComponent {}
